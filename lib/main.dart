2ca4427e8b12c2267316854249bca60f9caeba85
yusame0308 <yu.starts.yu@gmail.com>
2021-10-29 13:04:38 +0900

    create main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:tasks/models/task.dart';
-import 'package:tasks/viewModels/task_controller.dart';
 
 void main() {
-  runApp(ProviderScope(child: TasksApp()));
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-final taskProvider = StateNotifierProvider<TaskStateNotifier, Task>(
-    (ref) => TaskStateNotifier());
-List<Task> tasks = [];
-
-class TasksApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,40 +17,7 @@ class TasksApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("たすくす"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
-        child: Icon(Icons.add),
-        onPressed: () {
-          tasks.add(Task(id: "id", title: "test"));
-        },
-      ),
-      body: TaskList(),
-    );
-  }
-}
-
-class TaskList extends HookWidget {
-  @override
-  Widget build(BuildContext context) {
-    final state = useProvider(taskProvider);
-    return ListView.builder(
-      itemBuilder: (BuildContext context, int index) {
-        return Text(state.title);
-      },
-      itemCount: tasks.length,
+      home: Container(),
     );
   }
 }
